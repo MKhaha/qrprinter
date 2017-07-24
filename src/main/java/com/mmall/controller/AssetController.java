306993c2b7373f30fc5737f8704cfea0d5dbc2f5
guotao <xieguotao@ywgroup.com.cn>
2017-07-24 15:11:27 +0800

    打印服务在调试时被注释，未开启

--- src/main/java/com/mmall/controller/AssetController.java
+++ src/main/java/com/mmall/controller/AssetController.java
@@ -25,10 +25,7 @@ public class AssetController {
     @ResponseBody
     @Frequency(name = "printQRcode", time = 3, limit = 1)
     public ServerResponse<String> printQRcode(String assetId, String assetName) {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
-        System.out.println("current time : " + simpleDateFormat.format(new Date()));
-        return ServerResponse.createBySuccessMessage("adfasdfasdfasdf");
-        //return iAssetPrinter.assetPrint(assetId, assetName);
+        return iAssetPrinter.assetPrint(assetId, assetName);
     }
 
 }
